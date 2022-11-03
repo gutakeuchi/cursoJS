d8a45a5551dfab4d70fdc1155187729666ad66dc
gutakeuchi <gustavo.takeuchi87@gmail.com>
2022-11-02 23:08:10 -0300

    atualizado

--- aula 10/index.js
+++ aula 10/index.js
@@ -1,3 +1,4 @@
+// String, Number,  undefined, null, boolean, symbol
 const nome = 'Luiz'; //string
 const nome1 = "Luiz"; //string
 const nome2 = `Luiz`; //string 
@@ -5,6 +6,16 @@ const num1 = 10; //number
 const num2 = 10.52; //number
 let nomeAluno; // undefined = não aponta pra lugar nenhuma na memória
 const sobrenomeAluno = null; // Nulo -> não aponta pra local nenhuma na memória
-const boolean = false; // Boolean = true, false (lógico)
+const aprovado = false; // Boolean = true, false (lógico)
+
+let a = 2;
+const b = a;
+
+console.log(a, b); // 2, 2
+
+a  = 3;
+
+console.log(a, b); // 3, 2
+
 
 
---
+++ aula 11/index.js
@@ -0,0 +1,25 @@
+/**
+ * Aritméticos
+ * + Adição / Concatenação (- / *)
+ * ** Potenciação
+ * % Resto da divisão
+ * Prioridade 
+ ()
+ **
+ * / %
+ + - 
+ */
+
+/*
+    Incremento = ++
+    Decremento = --
+*/
+
+//Operadores de Atribuição 
+// *= , **= , += , -=
+
+// NaN - Not a number parseInt (inteiro), parseFloat (decimais), Number
+const num1 = 10;
+const num2 = Number('5');
+console.log(num1 + num2);
+console.log(typeof num2);
