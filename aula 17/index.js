2fef9a1775858446df543675cfa802b9666d2716
gutakeuchi <gustavo.takeuchi87@gmail.com>
2022-12-11 19:07:51 -0300

    aula sobre arrays e functions

---
+++ aula 17/index.js
@@ -0,0 +1,41 @@
+//              012345678910
+const nome = 'Luiz Otávio'
+
+//               0       1        2
+let alunos = ['Luiz', 'Maria', 'João'];
+alunos = 123;
+
+console.log(typeof alunos);
+console.log(alunos instanceof Array);
+
+// alunos.push('Luiza');
+// alunos.push('Eduardo');
+// console.log(alunos.slice(0, -2));
+
+// console.log(alunos[50]);
+
+// delete alunos[1];
+// console.log(alunos[1]);
+
+// const removido = alunos.shift();
+// console.log(removido);
+// console.log(alunos);
+
+// alunos.unshift('Luiza'); // Adiciona no começo
+// alunos.unshift('Fábio');
+
+
+
+//alunos.push('Luiza'); // Adiciona no fim
+//alunos.push('Fábio');
+
+//alunos[0] = 'Eduardo'; // Altera
+//alunos[3]  = 'Luiza'; // Adicionando
+
+//alunos[alunos.length] = 'Luiza'; // Adiciona no fim
+//alunos[alunos.length] = 'Fábio';
+//alunos[alunos.length] = 'Luana';
+
+//console.log(alunos);
+//console.log(alunos[0]);
+//console.log(alunos[2]);
