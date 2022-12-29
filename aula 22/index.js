212fd3728068e0072af6743cdb7f1283a3e2ff85
gutakeuchi <gustavo.takeuchi87@gmail.com>
2022-12-29 19:56:17 -0300

    atualizado do dia 29/11/22

---
+++ aula 22/index.js
@@ -0,0 +1,14 @@
+/*
+Operadores de comparação 
+> maior que 
+>= maior que ou igual a 
+< menor que 
+<= menor que ou igual a 
+=== igualdade estrita (valor e tipo)
+!== diferente estrito (valor e tipo)
+*/  
+const n1 = 10;
+const n2 = '10';
+
+
+console.log(n1 !== n2);
---
+++ aula 23/index.js
@@ -0,0 +1,14 @@
+/* Operadores Lógicos
+&& -> AND -> Todas as expressões precisam ser verdadeiras para retornar true 
+|| -> OR -> OU 
+! -> NOT -> NÃO
+*/
+
+// const expressaoAnd = true && true && true && true;
+// const expressaoOr = true || false || true || true;
+// const usuario = 'Luiz';
+// const senha = 123456;
+
+// const vaiLogar = usuario === 'Luiz' && senha === 12345;
+console.log(!true);
+
